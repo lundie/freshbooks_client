d3a859da0cc2be2274db99ff703962e7cdce179f
Jesse Herrick <me@jesse.codes>
2015-07-23 20:02:56 -0400

    Initialize Freshbook API client.

---
+++ lib/freshbooks.rb
@@ -0,0 +1,5 @@
+require "freshbooks/version"
+
+module Freshbooks
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'freshbooks'
+
+require 'minitest/autorun'
